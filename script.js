9d8296f96d427dc3c935bb3abf8de098de7d090c
prxybb <ncklmps@gmail.com>
2024-07-17 01:58:52 -0500

    window2

--- script.js
+++ script.js
@@ -6,30 +6,53 @@ const windowElement = document.getElementById('window1');
 const headerElement = windowElement.querySelector('.header');
 const resizeHandle = windowElement.querySelector('.resize-handle');
 
+// Function to handle dragging
 headerElement.addEventListener('mousedown', (e) => {
     isDragging = true;
     offsetX = e.clientX - windowElement.offsetLeft;
     offsetY = e.clientY - windowElement.offsetTop;
+    document.addEventListener('mousemove', handleDrag);
+    document.addEventListener('mouseup', stopDrag);
 });
 
-document.addEventListener('mouseup', () => {
-    isDragging = false;
-    isResizing = false;
-});
-
-document.addEventListener('mousemove', (e) => {
+function handleDrag(e) {
     if (isDragging) {
-        windowElement.style.left = \`\${e.clientX - offsetX}px\`;
-        windowElement.style.top = \`\${e.clientY - offsetY}px\`;
-    } else if (isResizing) {
-        windowElement.style.width = \`\${e.clientX - windowElement.offsetLeft}px\`;
-        windowElement.style.height = \`\${e.clientY - windowElement.offsetTop}px\`;
+        windowElement.style.left = `${e.clientX - offsetX}px`;
+        windowElement.style.top = `${e.clientY - offsetY}px`;
     }
-});
+}
 
+function stopDrag() {
+    isDragging = false;
+    document.removeEventListener('mousemove', handleDrag);
+    document.removeEventListener('mouseup', stopDrag);
+}
+
+// Function to handle resizing
 resizeHandle.addEventListener('mousedown', (e) => {
     isResizing = true;
+    offsetX = e.clientX;
+    offsetY = e.clientY;
+    document.addEventListener('mousemove', handleResize);
+    document.addEventListener('mouseup', stopResize);
 });
+
+function handleResize(e) {
+    if (isResizing) {
+        const width = windowElement.offsetWidth + (e.clientX - offsetX);
+        const height = windowElement.offsetHeight + (e.clientY - offsetY);
+        windowElement.style.width = `${width}px`;
+        windowElement.style.height = `${height}px`;
+        offsetX = e.clientX;
+        offsetY = e.clientY;
+    }
+}
+
+function stopResize() {
+    isResizing = false;
+    document.removeEventListener('mousemove', handleResize);
+    document.removeEventListener('mouseup', stopResize);
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
